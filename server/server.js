486e3665b3e2fdfdf301b11e596cff134acd9fbd
Aravindh <aravindhprabu2005@gmail.com>
2025-12-16 11:29:24 +0530

    port env

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const path = require("path");
 const serverless = require("serverless-http")
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
