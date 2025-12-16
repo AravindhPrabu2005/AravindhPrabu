b66ab500e6032ed08b4b0f9677b972873568a454
Aravindh <aravindhprabu2005@gmail.com>
2025-12-16 11:39:37 +0530

    mongoDB driver correction

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("MongoDB Connection Error:", err));
 
