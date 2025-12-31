eb7be2aa72dbe9d2beed616696d7e02cbb3c3366
Aravindh <aravindhprabu2005@gmail.com>
2025-12-31 22:38:15 +0530

    hiding leetcode

--- client/src/components/axiosInstance.js
+++ client/src/components/axiosInstance.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://dzc0ibd8dc.execute-api.ap-south-1.amazonaws.com/dev',  
+    baseURL: 'http://localhost:5000',  
 });
 // aws url : https://dzc0ibd8dc.execute-api.ap-south-1.amazonaws.com/dev
 // local url: http://localhost:5000
-// render url: https://aravindhprabu-portfolio.onrender.com
 
 export default axiosInstance
